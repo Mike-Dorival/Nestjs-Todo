3a292650700dd05f1b6d47aad3fbcbc9c8244131
Mirosz-Mike <mikedorival@gmail.com>
2020-05-05 20:08:45 +0200

    feat: add authentification and hash password

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,22 @@
+import { IsString, MinLength, MaxLength, Matches } from "class-validator";
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: "password not conform",
+  })
+  password: string;
+}
+
+// /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/ veut dire :
+// Passwords will contain at least 1 upper case letter
+// Passwords will contain at least 1 lower case letter
+// Passwords will contain at least 1 number or special character
+// There is no length validation (min, max) in this regex!
