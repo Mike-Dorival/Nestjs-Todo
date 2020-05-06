5a0c779a84086b2b6f841bc0a7c6fc080b22f39e
Mirosz-Mike <mikedorival@gmail.com>
2020-05-06 16:05:18 +0200

    feat: add jwt

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,24 +1,14 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Delete,
-  Patch,
-  Query,
-  UsePipes,
-  ValidationPipe,
-  ParseIntPipe,
-} from "@nestjs/common";
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe, ParseIntPipe, UseGuards } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
 import { TaskStatusValidationPipe } from "./pipes/task-status-validation.pipe";
 import { Task } from "./task.entity";
 import { TaskStatus } from "./task-status.enum";
+import { AuthGuard } from "@nestjs/passport";
 
 @Controller("tasks")
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
@@ -39,10 +29,7 @@ export class TasksController {
   }
 
   @Patch("/:id/status")
-  updateTaskById(
-    @Param("id", ParseIntPipe) id: number,
-    @Body("status", TaskStatusValidationPipe) status: TaskStatus
-  ): Promise<Task> {
+  updateTaskById(@Param("id", ParseIntPipe) id: number, @Body("status", TaskStatusValidationPipe) status: TaskStatus): Promise<Task> {
     return this.tasksService.updateTaskById(id, status);
   }
 
